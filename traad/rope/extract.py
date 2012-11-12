15a97727dbbfde426f6e9733b5039b134c69905c
Austin Bingham <austin.bingham@gmail.com>
2012-11-12 21:55:23 +0100

    Refactored extract functions.

---
+++ traad/rope/extract.py
@@ -0,0 +1,69 @@
+import rope.refactor.extract
+
+import traad.trace
+from traad.rope.validate import validate
+
+class ExtractFunctions:
+    """This extract related functions of the rope interface.
+
+    A base for RopeInterface.
+
+    """
+
+    @validate
+    def _extract(self, path, name, start_offset, end_offset, cls):
+        '''Core extract-* method, parameterized on the class of
+        the extraction.
+        '''
+
+        path = self._to_relative_path(path)
+
+        extractor = cls(
+            self.proj,
+            self.proj.get_resource(path),
+            start_offset,
+            end_offset)
+
+        self.proj.do(extractor.get_changes(name))
+
+    @traad.trace.trace
+    def extract_method(self, name, path, start_offset, end_offset):
+        '''Extract a method.
+
+        ``path`` may be absolute or relative. If ``path`` is relative,
+        then it must to be relative to the root of the project.
+
+        Args:
+          new_name: The name for the new method.
+          path: The path of the resource containing the code.
+          start_offset: The starting offset of the region to extract.
+          end_offset: The end (one past the last character) of the
+            region to extract.
+        '''
+
+        self._extract(path,
+                      name,
+                      start_offset,
+                      end_offset,
+                      rope.refactor.extract.ExtractMethod)
+
+    @traad.trace.trace
+    def extract_variable(self, name, path, start_offset, end_offset):
+        '''Extract a variable.
+
+        ``path`` may be absolute or relative. If ``path`` is relative,
+        then it must to be relative to the root of the project.
+
+        Args:
+          new_name: The name for the new variable.
+          path: The path of the resource containing the code.
+          start_offset: The starting offset of the region to extract.
+          end_offset: The end (one past the last character) of the
+            region to extract.
+        '''
+
+        self._extract(path,
+                      name,
+                      start_offset,
+                      end_offset,
+                      rope.refactor.extract.ExtractVariable)
--- traad/rope/interface.py
+++ traad/rope/interface.py
@@ -3,11 +3,11 @@ import os
 import rope.base.project
 import rope.contrib.codeassist
 import rope.refactor.change_signature
-import rope.refactor.extract
 import rope.refactor.importutils
 import rope.refactor.rename
 
 import traad.trace
+from traad.rope.extract import ExtractFunctions
 from traad.rope.history import HistoryFunctions
 from traad.rope.log import log
 from traad.rope.validate import validate
@@ -33,7 +33,8 @@ def get_all_resources(proj):
         if res.is_folder():
             todo.extend((child.path for child in res.get_children()))
 
-class RopeInterface(HistoryFunctions):
+class RopeInterface(ExtractFunctions,
+                    HistoryFunctions):
     def __init__(self,
                  project_dir):
         self.proj = rope.base.project.Project(project_dir)
@@ -67,65 +68,6 @@ class RopeInterface(HistoryFunctions):
             is_folder).
         '''
         return list(get_all_resources(self.proj))
-
-
-    @validate
-    def _extract(self, path, name, start_offset, end_offset, cls):
-        '''Core extract-* method, parameterized on the class of
-        the extraction.
-        '''
-
-        path = self._to_relative_path(path)
-
-        extractor = cls(
-            self.proj,
-            self.proj.get_resource(path),
-            start_offset,
-            end_offset)
-
-        self.proj.do(extractor.get_changes(name))
-
-    @traad.trace.trace
-    def extract_method(self, name, path, start_offset, end_offset):
-        '''Extract a method.
-
-        ``path`` may be absolute or relative. If ``path`` is relative,
-        then it must to be relative to the root of the project.
-
-        Args:
-          new_name: The name for the new method.
-          path: The path of the resource containing the code.
-          start_offset: The starting offset of the region to extract.
-          end_offset: The end (one past the last character) of the
-            region to extract.
-        '''
-
-        self._extract(path,
-                      name,
-                      start_offset,
-                      end_offset,
-                      rope.refactor.extract.ExtractMethod)
-
-    @traad.trace.trace
-    def extract_variable(self, name, path, start_offset, end_offset):
-        '''Extract a variable.
-
-        ``path`` may be absolute or relative. If ``path`` is relative,
-        then it must to be relative to the root of the project.
-
-        Args:
-          new_name: The name for the new variable.
-          path: The path of the resource containing the code.
-          start_offset: The starting offset of the region to extract.
-          end_offset: The end (one past the last character) of the
-            region to extract.
-        '''
-
-        self._extract(path,
-                      name,
-                      start_offset,
-                      end_offset,
-                      rope.refactor.extract.ExtractVariable)
 
     @traad.trace.trace
     @validate
