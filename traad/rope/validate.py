9810793e661dabe3cfa78f450bebe75b033f93a0
Austin Bingham <austin.bingham@emerson.com>
2012-11-30 16:09:35 +0100

    Added a bit more logging around validation.

--- traad/rope/validate.py
+++ traad/rope/validate.py
@@ -9,4 +9,5 @@ def validate(f, self, *args, **kwargs):
     "Call self.proj.validate() before running the function."
     log.info('Validating')
     self.proj.validate()
+    log.info('Done validating')
     return f(self, *args, **kwargs)
