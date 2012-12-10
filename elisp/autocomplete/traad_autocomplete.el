76c55471a4a61b24b9f36cfe76f7b05bf7b8085c
Austin Bingham <austin.bingham@emerson.com>
2012-12-10 09:23:32 +0100

    updated autocomplete example

--- elisp/autocomplete/traad_autocomplete.el
+++ elisp/autocomplete/traad_autocomplete.el
@@ -3,43 +3,26 @@
 
 (require 'auto-complete)
 
-(defun ac-traad-find ()
-  "Find the starting point of the 'thing' at point."
-  (require 'thingatpt)
-  (let ((symbol (car-safe (bounds-of-thing-at-point 'symbol))))
-    (if (null symbol)
-        (if (string= "." (buffer-substring (- (point) 1) (point)))
-            (point)
-          nil)
-      symbol)))
-
-(defvar ac-traad-cache nil
-  "Holds the results of the last traad-code-assist.")
-
 (defun ac-traad-candidates ()
   "Get the list of completions at point."
-  (let* ((real-point (ac-traad-find))
-         (pt (if real-point real-point (point))))
-    (setq ac-traad-cache (traad-code-assist pt))
-    (mapcar 'car ac-traad-cache)))
+  (mapcar 'car (traad-code-assist (point))))
 
 (defun ac-traad-documentation (sym)
   "Look up symbol documentation in the cache."
-  (let ((entry (assoc sym ac-traad-cache)))
-    (if entry (cadr entry))))
+  (traad-get-doc (point)))
 
 ;; The autocomplete source for traad
 (ac-define-source traad
   '((depends traad)
     (candidates . ac-traad-candidates)
+    (cache)
     (document . ac-traad-documentation)
     (symbol . "s")
     (requires . 0)))
 
 (defun ac-traad-setup ()
   "Add ac-source-traad to autocomplete list."
-  ;(setq ac-sources (append '(ac-source-traad) ac-sources)))
-  (setq ac-sources '(ac-source-traad)))
+  (setq ac-sources (append '(ac-source-traad) ac-sources)))
 
 ;; Insert the traad source in python mode.
 (add-hook 'python-mode-hook 'ac-traad-setup)
