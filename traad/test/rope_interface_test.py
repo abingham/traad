653322e254a1ecc097ba2353786db18e9dc2bd06
Austin Bingham <austin.bingham@gmail.com>
2013-04-20 18:10:42 +0200

    rope-interface tests now using common test module.

--- traad/test/rope_interface_test.py
+++ traad/test/rope_interface_test.py
@@ -4,30 +4,16 @@ import shutil
 import time
 import unittest
 
-from ..rope.interface import RopeInterface
+from traad.rope.interface import RopeInterface
+from traad.test import common
 
-THIS_DIR = os.path.split(__file__)[0]
-ACTIVE_DIR = os.path.join(THIS_DIR, 'active')
-PROJECT_DIR = os.path.join(THIS_DIR, 'projects')
 
 class Tests(unittest.TestCase):
     def setUp(self):
-        try:
-            shutil.rmtree(ACTIVE_DIR)
-        except OSError:
-            pass
-
-        os.mkdir(ACTIVE_DIR)
-
-        shutil.copytree(
-            os.path.join(PROJECT_DIR, 'main'),
-            os.path.join(ACTIVE_DIR, 'main'))
-
-        self.ri = RopeInterface(
-            os.path.join(ACTIVE_DIR, 'main'))
+        self.ri = common.activate(['main'])
 
     def tearDown(self):
-        shutil.rmtree(ACTIVE_DIR)
+        common.deactivate()
 
     def test_find_occurences(self):
         # Find occurrences of the Foo class
