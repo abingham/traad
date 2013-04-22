bdbf29a8a5dd0eebbc587b18b2bf1472d346de97
Thomas Frössman <thomasf@jossystem.se>
2013-04-22 23:20:49 +0200

    Don't switch to traad buffer when starting process

--- elisp/traad.el
+++ elisp/traad.el
@@ -103,7 +103,7 @@ after successful refactorings."
     (read-directory-name "Directory: ")))
   (traad-close)
   (let ((proc-buff (get-buffer-create "*traad-server*")))
-    (switch-to-buffer proc-buff)
+    (set-buffer proc-buff)
     (erase-buffer)
     (let* ((program (if (listp traad-server-program) 
 			traad-server-program 
