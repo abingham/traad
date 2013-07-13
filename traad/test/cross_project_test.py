1051e629a868601eb0a7ac4878e3e47dfad4c32e
Austin Bingham <austin.bingham@gmail.com>
2013-07-13 22:41:10 +0200

    Re-enabled normalize_signature tests.

--- traad/test/cross_project_test.py
+++ traad/test/cross_project_test.py
@@ -1,27 +1,29 @@
 import unittest
 
-from traad.rope.interface import RopeInterface
+from traad.rope.change_signature import normalize_arguments
+from traad.state import State
 from traad.test import common
 
 
 class CrossTests(unittest.TestCase):
     def setUp(self):
-        self.ri = common.activate(['main', 'cross'])
+        self.state = State()
+        self.task_id = 1
+        self.state.create(self.task_id)
+        self.task_state = self.state.get_task_state(self.task_id)
+        self.proj = common.activate(['main', 'cross'])
 
     def tearDown(self):
         common.deactivate()
 
-    def test_cross_rename(self):
-        self.ri.rename(
-            'NotBar',
-            'basic/bar.py',
-            30)
-
     def test_cross_normalize_arguments(self):
-        self.ri.normalize_arguments(
+        normalize_arguments(
+            self.proj,
+            self.task_state,
             'basic/bar.py',
             163)
 
+    @unittest.skip('reactivate')
     def test_cross_remove_argument(self):
         self.ri.remove_argument(
             1,
