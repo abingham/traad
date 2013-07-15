5549bfdb9c73254c03ed38768109e91406b8c8dd
Austin Bingham <austin.bingham@gmail.com>
2013-07-15 06:12:24 +0200

    Working to get autocomplete working again.

--- elisp/traad.el
+++ elisp/traad.el
@@ -119,7 +119,7 @@ after successful refactorings."
 	   (proc (apply #'start-process "traad-server" proc-buff program+args))
 	   (cont 1))
       (while cont
-    (set-process-query-on-exit-flag proc nil)
+        (set-process-query-on-exit-flag proc nil)
 	(accept-process-output proc 0 100 t)
 	(let ((port-str (with-current-buffer proc-buff
 			  (buffer-string))))
