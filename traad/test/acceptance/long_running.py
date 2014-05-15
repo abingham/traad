e187c83b481843307e48347d917ab64b2fcc5a8b
Austin Bingham <austin.bingham@gmail.com>
2014-05-15 11:48:10 +0200

    long-running test now using unittest framework

--- traad/test/acceptance/long_running.py
+++ traad/test/acceptance/long_running.py
@@ -1,28 +1,34 @@
 import time
+import unittest
 
 import webtest
 
 import traad.server
 
 
-try:
-    traad_app = traad.server.make_app('.')
-    app = webtest.TestApp(traad_app)
+class LongRunningTest(unittest.TestCase):
+    def test_long_running(self):
+        try:
+            traad_app = traad.server.make_app('.')
+            app = webtest.TestApp(traad_app)
 
-    resp = app.post_json(
-        '/test/long_running',
-        {'message': 'Test message!'})
+            resp = app.post_json(
+                '/test/long_running',
+                {'message': 'Test message!'})
 
-    task_id = resp.json['task_id']
+            task_id = resp.json['task_id']
 
-    results = set()
+            results = set()
 
-    start_time = time.time()
-    while time.time() - start_time < 15:
-        resp = app.get('/task/{}'.format(task_id))
-        results.add(resp.body)
-        time.sleep(0.1)
+            start_time = time.time()
+            while time.time() - start_time < 15:
+                resp = app.get('/task/{}'.format(task_id))
+                results.add(resp.body)
+                time.sleep(0.1)
 
-    assert len(results) == 11
-finally:
-    traad.server.stop_app(traad_app)
+            self.assertEqual(len(results), 11)
+        finally:
+            traad.server.stop_app(traad_app)
+
+if __name__ == '__main__':
+    unittest.main()
