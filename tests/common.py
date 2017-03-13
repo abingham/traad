d71a71550bcb8e7c1de2d283b38e52c9371bb03d
Austin Bingham <austin@sixty-north.com>
2017-03-13 22:26:09 +0100

    Started pytest conversion for history tests.

---
+++ tests/common.py
@@ -0,0 +1,65 @@
+import paths
+
+from filecmp import dircmp
+from itertools import chain
+import os
+
+try:
+    from io import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+
+def diff_report(dc):
+    '''Generate a report of differences.
+
+    Stolen directly from `filecmp.dircmp` and modified to be non-insane.
+
+    Args:
+        dc: A `dircmp` object.
+
+    Returns: a string with the report.
+    '''
+    # Output format is purposely lousy
+    sio = StringIO()
+
+    if dc.left_only:
+        dc.left_only.sort()
+        sio.write(u' '.join(chain(['Only in', dc.left, ':'], dc.left_only)))
+    if dc.right_only:
+        dc.right_only.sort()
+        sio.write(u' '.join(chain(['Only in', dc.right, ':'], dc.right_only)))
+    if dc.same_files:
+        dc.same_files.sort()
+        sio.write(u' '.join(chain(['Identical files:'], dc.same_files)))
+    if dc.diff_files:
+        dc.diff_files.sort()
+        sio.write(u' '.join(chain(['Differing files:'], dc.diff_files)))
+    if dc.funny_files:
+        dc.funny_files.sort()
+        sio.write(u' '.join(chain(['Trouble with common files:'], dc.funny_files)))
+    if dc.common_dirs:
+        dc.common_dirs.sort()
+        sio.write(u' '.join(chain(['Common subdirectories:'], dc.common_dirs)))
+    if dc.common_funny:
+        dc.common_funny.sort()
+        sio.write(u' '.join(chain(['Common funny cases:'], dc.common_funny)))
+
+    sio.seek(0)
+    return sio.read()
+
+
+def compare_projects(canned_project,
+                     top_level_dir,
+                     project):
+    dc = dircmp(os.path.join(paths.PROJECT_DIR,
+                             canned_project),
+                os.path.join(paths.ACTIVE_DIR,
+                             top_level_dir,
+                             project))
+
+    if any([dc.left_only,
+            dc.right_only,
+            dc.diff_files]):
+        dr = diff_report(dc)
+        raise ValueError(dr)
