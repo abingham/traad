60290022b40c7d5ea3a83a845b078ba940275355
Austin Bingham <austin@sixty-north.com>
2017-03-14 13:16:47 +0100

    Removed unittest-based tests.

--- traad/test/projects/basic/overrides.py
+++
@@ -1,7 +0,0 @@
-class BaseClass:
-    def base_func(self):
-        return 1
-
-class SubClass(BaseClass):
-    def base_func(self):
-        return 2
--- traad/test/projects/cross_use_bar_remove_argument/use_bar.py
+++
@@ -1,5 +0,0 @@
-from basic.bar import Bar
-
-def use_bar():
-    b = Bar()
-    return b.some_func()
