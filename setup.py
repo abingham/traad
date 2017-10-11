1584bba013ff137c4526d415578dd4b7cf1806bd
Austin Bingham <austin@sixty-north.com>
2017-10-11 17:24:07 +0200

    bumped version to 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='traad',
-    version='0.11',
+    version='1.0.0',
     packages=find_packages(),
 
     # metadata for upload to PyPI
