7750feae3f2fdd478c26ee5dcb3c13f50c4d3eaf
Austin Bingham <austin.bingham@gmail.com>
2018-05-06 12:54:21 -0500

    Moved version info into version.py.
    Bumped version.

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import io
+import os
 from setuptools import setup, find_packages
 import sys
 
@@ -9,9 +11,35 @@ install_requires = [
 if sys.version_info < (3, 4):
     install_requires.append('enum34')
 
+
+def local_file(*name):
+    return os.path.join(
+        os.path.dirname(__file__),
+        *name)
+
+
+def read(name, **kwargs):
+    with io.open(
+        name,
+        encoding=kwargs.get("encoding", "utf8")
+    ) as handle:
+        return handle.read()
+
+
+# This is unfortunately duplicated from scripts/cosmic_ray_tooling.py. I
+# couldn't find a way to use the original version and still have tox
+# work...hmmm...
+def read_version(version_file):
+    "Read the `(version-string, version-info)` from `version.py`."
+    local_vars = {}
+    with open(version_file) as handle:
+        exec(handle.read(), {}, local_vars)  # pylint: disable=exec-used
+    return (local_vars['__version__'], local_vars['__version_info__'])
+
+
 setup(
     name='traad',
-    version='3.0.1',
+    version=read_version(local_file('traad', 'version.py'))[0],
     packages=find_packages(),
 
     # metadata for upload to PyPI
